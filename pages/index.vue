5c0003da60dc618e4284fe96815667444fd1ec26
Faiza Aulia Rahma Putra <faizaauliarp@gmail.com>
2023-08-22 13:45:51 +0700

    route parameters & navigation link

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Belajar Nuxt</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
     <h2>This is Index page</h2>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos velit ipsam, eveniet
       eum odio quam dicta cupiditate architecto quaerat sint veritatis incidunt minima laboriosam nulla
---
+++ pages/products/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>This is detail of product {{ id }}</p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+</script>
+<style scoped>
+
+</style>
