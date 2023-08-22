52f568e76efb4251aa0405d8d7d5df42de39cea4
Faiza Aulia Rahma Putra <faizaauliarp@gmail.com>
2023-08-22 14:33:25 +0700

    error handling

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -23,6 +23,10 @@
   const url = 'https://fakestoreapi.com/products/' + id
 
   const { data: product } = await useFetch(url, { key: id })
+
+  if (!product.value) {
+    throw createError({ statusCode: 404, message: 'Product not found', fatal: true })
+  }
 </script>
 
 <style scoped>
