400953d9eafa5503420563c3e13b0d5964e4df21
Faiza Aulia Rahma Putra <faizaauliarp@gmail.com>
2023-08-22 13:39:23 +0700

    add some new pages

---
+++ pages/products/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>This is Products page</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos velit ipsam, eveniet
+      eum odio quam dicta cupiditate architecto quaerat sint veritatis incidunt minima laboriosam nulla
+      illum in sequi? Minus?
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
